f25662708eabe3eb0052b2e7aa7d46282b0ced11
michelllim <2201198e@student.tp.edu.sg>
2024-01-16 16:31:59 +0800

    Michell Entities

---
+++ HealthMonitoringWebsite/Shared/Domain/Consultation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoringWebsite.Shared.Domain
+{
+    public class Consultation: BaseDomainModel
+    {
+        public int ConsultationID { get; set; }
+        public DateOnly ConsultationDate { get; set; }
+        public TimeOnly ConsultationStartTime { get; set; }
+        public TimeOnly ConsultationEndTime { get; set; }
+        public float? ConsultationPrice { get; set; }
+        public string? ConsultationContent { get; set; }
+        public string? ConsultationLocation { get; set; }
+        public virtual VitalSignsRecord? VitalSignsRecord { get; set; }  
+    }
+}
---
+++ HealthMonitoringWebsite/Shared/Domain/Diagnosis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoringWebsite.Shared.Domain
+{
+    public abstract class Diagnosis: BaseDomainModel
+    {
+        public int DiagnosisID { get; set; }
+        public DateOnly DiagnosisDate { get; set; }
+        public TimeOnly DiagnosisTime { get; set; }
+        public string? BodyPart { get; set; }
+        public string? Symptoms { get; set; }
+        public string? Conditions { get; set; }
+        public virtual Consultation? Consultation { get; set; }  
+
+    }
+}
