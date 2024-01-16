b373799d4e0ccbfd107093f912c568c21a77972d
michelllim <2201198e@student.tp.edu.sg>
2024-01-16 14:33:38 +0800

    Created basic landing page

---
+++ HealthMonitoringWebsite/Server/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthMonitoringWebsite.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
