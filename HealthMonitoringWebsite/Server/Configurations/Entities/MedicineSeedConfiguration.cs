00546382e2c7101992a03fa711bb96b0393c3da3
peinee5 <peinee2005@gmail.com>
2024-01-17 11:41:05 +0800

    (Staff, Medicine)Seed Configuration

---
+++ HealthMonitoringWebsite/Server/Configurations/Entities/MedicineSeedConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using HealthMonitoringWebsite.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+//using System.Ling;
+using System.Threading.Tasks;
+
+namespace HealthMonitoringWebsite.Server.Configurations.Entities
+{
+    public class MedicineSeedConfiguration : IEntityTypeConfiguration<Medicine>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Medicine> builder)
+        {
+
+            builder.HasData(
+                new Medicine
+                {
+                    MedicineID = 1,
+                    MName = "Benzonatate",
+                    MStrength = "100mg",
+                    MType = "Oral Capsule",
+                    MTotalAmount = 0,
+                    MInstructions = "Adults and children 10 years of age and older—100 milligrams (mg) three times a day. " +
+                    "Do not take more than 200 mg at one time or more than 600 mg per day.\r\nChildren younger than 10 years of age—Use is not recommended",
+                    MPurpose = "It is used to relieve coughing.",
+                    MExpiryDate = DateTime.Now.AddMonths(3)
+                },
+                new Medicine
+                {
+                    MedicineID = 2,
+                    MName = "Acebutolol",
+                    MStrength = "200 mg",
+                    MType = "Oral Capsule",
+                    MTotalAmount = 0,
+                    MInstructions = "Initial dose: 400 mg orally per day in 1 to 2 divided doses\r\nMaintenance dose: 400 to 800 mg orally per day\r\n" +
+                    "You should not use acebutolol if you have a serious heart condition such as \"AV block\" (2nd or 3rd degree), severe heart failure," +
+                    " or slow heartbeats that have caused you to faint.",
+                    MPurpose = "It is a beta blocker for the treatment of hypertension and arrhythmias (lowers blood pressure and irregular heartbeat).",
+                    MExpiryDate = DateTime.Now.AddMonths(3)
+                }
+                );
+
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ HealthMonitoringWebsite/Server/Configurations/Entities/StaffSeedConfiguration.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using HealthMonitoringWebsite.Shared.Domain;
+using System.Collections.Generic;
+
+namespace HealthMonitoringWebsite.Server.Configurations.Entities
+{
+    public class StaffSeedConfiguration : IEntityTypeConfiguration<Staff>
+    {
+        public void Configure(EntityTypeBuilder<Staff> builder)
+        {
+            builder.HasData(
+                new Staff
+                {
+                    StaffID = 1,
+                    StaffName = "Chen Pei Nee",
+                    StaffContactNumber = "89547968",
+                    StaffRole = "Doctor",
+                    StaffSpecialization = "Cardiologist",
+                    Email = "peinee2005@gmail.com",
+                    Password = "DPeiNee123"
+                },
+                new Staff
+                {
+                    StaffID = 2,
+                    StaffName = "Tom Liew",
+                    StaffContactNumber = "68902727",
+                    StaffRole = "Doctor",
+                    StaffSpecialization = "Pulmonologist",
+                    Email = "tomLiew@gmail.com",
+                    Password = "DTom123"
+                },
+                new Staff
+                {
+                    StaffID = 3,
+                    StaffName = "Amy Tang",
+                    StaffContactNumber = "98213410",
+                    StaffRole = "Nurse",
+                    StaffSpecialization = "Neonatal Nurse",
+                    Email = "amy123@gmail.com",
+                    Password = "NAmy123"
+                },
+                new Staff
+                {
+                    StaffID = 4,
+                    StaffName = "Vali Thamoi",
+                    StaffContactNumber = "92859335",
+                    StaffRole = "Nurse",
+                    StaffSpecialization = "Psychiatric Nurse",
+                    Email = "valiT@gmail.com",
+                    Password = "NVali123"
+                },
+                new Staff
+                {
+                    StaffID = 5,
+                    StaffName = "John",
+                    StaffContactNumber = "86547733",
+                    StaffRole = "Administrative Staff",
+                    StaffSpecialization = "Receptionists",
+                    Email = "johnT@gmail.com",
+                    Password = "AJohn123"
+                },
+                new Staff
+                {
+                    StaffID = 6,
+                    StaffName = "Tammy",
+                    StaffContactNumber = "85557763",
+                    StaffRole = "Medical Records Staff",
+                    StaffSpecialization = "Receptionists",
+                    Email = "Tammy@gmail.com",
+                    Password = "MRTammy123"
+                },
+                new Staff
+                {
+                    StaffID = 7,
+                    StaffName = "Tan Yan Ting",
+                    StaffContactNumber = "90683274",
+                    StaffRole = "Therapists",
+                    StaffSpecialization = "Physical Therapists",
+                    Email = "TYanTing@gmail.com",
+                    Password = "TYanTing123"
+                }
+                );
+        }
+    }
+}
