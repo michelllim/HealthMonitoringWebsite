f60545e9f02602320a2e30fc95420f7458846919
peinee5 <peinee2005@gmail.com>
2024-02-04 21:49:44 +0800

    Finished CSS Stylings

---
+++ SQLQuery1_useless.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO Medicines VALUES
+('T1V4,', 'Benzonatate','100mg','Oral Capsule','0', 'Adults and children 10 years of age and older—100 milligrams (mg) three times a day. 
+Do not take more than 200 mg at one time or more than 600 mg per day.\r\nChildren younger than 10 years of age—Use is not recommended.',
+'It is used to relieve coughing.',
+'DateTime.Now.AddMonths(3)'),
+('T2V9', 'Acebutolol','200 mg','Oral Capsule','0','Initial dose: 400 mg orally per day in 1 to 2 divided doses\r\nMaintenance dose: 400 to 800 mg orally per day\r\n
+You should not use acebutolol if you have a serious heart condition such as \"AV block\" (2nd or 3rd degree), severe heart failure,
+ or slow heartbeats that have caused you to faint.',
+'It is a beta blocker for the treatment of hypertension and arrhythmias (lowers blood pressure and irregular heartbeat).',
+'DateTime.Now.AddMonths(3)');
